7f2c67b5eabdd4e278e932cead0318fa39437460
Mwavuna <gregsonmwavuna3@gmail.com>
2025-05-02 00:09:42 +0300

    Another commit

---
+++ frontend/src/pages/login/login.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './login.scss'
+function login() {
+  return (
+    <div className='main'>
+        <div>
+        <div className='left'>
+        <h2>Hello WOrld</h2>
+        <p></p>
+        <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius tenetur quia iusto aliquam, voluptas distinctio iure a! Voluptates animi asperiores qui quas dignissimos quibusdam tempore! Delectus ipsam veritatis aut quasi?</span>
+        <button>Register</button>
+      </div>
+      <div className='right'>
+        <h2>Login</h2>
+        <form>
+            <input type="text" />
+            <input type="password" />
+            <button>Login</button>
+        </form>
+      </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default login
