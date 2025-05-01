aebd03183409b2662b99ae370383493dd1d39514
Mwavuna <gregsonmwavuna3@gmail.com>
2025-05-02 01:55:53 +0300

    Another commit

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -5,22 +5,22 @@ function login() {
     <div className="main-font main ">
       <div className="container">
         <div className="left">
-          <h2>Hello World</h2>
-          <p></p>
-          <span>
+          <h2 className="heading-font">Hello World</h2>
+          <p>
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
             tenetur quia iusto aliquam, voluptas distinctio iure a! Voluptates
             animi asperiores qui quas dignissimos quibusdam tempore! Delectus
             ipsam veritatis aut quasi?
-          </span>
-          <button>Register</button>
+          </p>
+          <span>Don't have an account ? </span>
+          <a>Register</a>
         </div>
         <div className="right">
           <h2>Login</h2>
           <form>
-            <input type="text" />
-            <input type="password" />
-            <button>Login</button>
+            <input type="text" placeholder="username" name="username" />
+            <input type="password" placeholder="password" name="password" />
+            <a>Login</a>
           </form>
         </div>
       </div>
