a021da0662b6cb59456d05d119c2ca3851bf9bbf
Mwavuna <gregsonmwavuna3@gmail.com>
2025-05-01 22:58:01 +0300

    Another commit

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ git push
 #deploy frontend to gh pages
 echo "Deploying frontend to production server..."
 npm --prefix frontend run build #build the dist folder in the frontend directory
-npx gh-pages -d frontend/dist -r htts://github.com/mwavuna/chatApp.git #push the dist folder in the frontend directory to the gh-pages branch of the repository
+npx gh-pages -d frontend/dist -r https://github.com/mwavuna/chatApp.git #push the dist folder in the frontend directory to the gh-pages branch of the repository
 
 echo "Deployment complete!"
 
